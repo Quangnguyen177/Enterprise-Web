7604c552ff725cb2073cd2a0b0b6e5f2d29cb6b2
ducmtgch200681 <ducmtgch200681@fpt.edu.vn>
2023-02-05 15:07:01 +0700

    Staff + Tag + Idea
    Create 3 model
    ApplicationDbContext update
    Testing the very first step

---
+++ COMP1640/Models/Tag.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace COMP1640.Models
+{
+    public class Tag
+    {
+        public int TagId { get; set; }
+
+        public string tag_name { get; set; }
+
+        //-------------------------
+        public ICollection<Idea> Ideas { get; set; }
+    }
+}
