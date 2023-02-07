6643e4b0b74792a74e844b1cf76dffcc91848696
Harris <89270256+BeDevH@users.noreply.github.com>
2023-02-07 18:50:40 +0700

    dashboard + signup staff

---
+++ COMP1640/Controllers/AdminController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP1640.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult dashboard()
+        {
+            return View();
+        }
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+    }
+}
