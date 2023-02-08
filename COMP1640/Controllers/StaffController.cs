d34b4cb8bca931b9c5020df775be68347dfd750d
DanTruongdev <89248193+DanTruongdev@users.noreply.github.com>
2023-02-09 00:17:21 +0700

    create idea function

---
+++ COMP1640/Controllers/StaffController.cs
@@ -0,0 +1,44 @@
+﻿using COMP1640.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace COMP1640.Controllers
+{
+    public class StaffController : Controller
+    {
+        private readonly ApplicationDbContext Db;
+        public StaffController(ApplicationDbContext context)
+        {
+            Db = context;
+        }
+        [HttpGet]
+        public IActionResult AddIdea()
+        {
+            
+            return View(Db.Tags.ToList());
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddIdea(string title, string content, string tagName, string isAnonymous)
+        {
+            Tag tag = Db.Tags.FirstOrDefault(t => t.tag_name == tagName);
+            Boolean anonynous = true;
+            if (isAnonymous == null) anonynous=false;
+            Idea newIdea = new Idea()
+            {
+                idea_title = title,
+                idea_content = content,
+                idea_anonymous = anonynous,
+                StaffId = 1,
+                TagId = tag.TagId,
+                created_date = DateTime.Now,
+            };
+            Db.Add(newIdea);
+            await Db.SaveChangesAsync();
+            return View(Db.Tags.ToList());
+        }
+        
+    }
+}
