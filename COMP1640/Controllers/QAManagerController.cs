2b523c33fadba043b0ca2f6c1b81e3b519260775
duong2208 <hakuhanzi@gmail.com>
2023-02-08 11:48:08 +0700

    CRUDTagMethod
    Read + Add + Update + Delete Tags Function
    BE

---
+++ COMP1640/Controllers/QAManagerController.cs
@@ -0,0 +1,69 @@
+﻿using COMP1640.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace COMP1640.Controllers
+{
+    public class QAManagerController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+
+        public QAManagerController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(context.Tags.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult AddTag()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddTag(Tag tag)
+        {
+            context.Tags.Add(tag);
+            context.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult DeleteTag(int? id)
+        {
+            var tag = context.Tags.Find(id);
+            context.Tags.Remove(tag);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult EditTag(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            return View(context.Tags.Find(id));
+        }
+
+        [HttpPost]
+        public IActionResult EditTag(Tag tag)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Tags.Update(tag);
+                context.SaveChanges();
+                TempData["Message"] = "Edit student successfully !";
+                return RedirectToAction(nameof(Index));
+            }
+            else
+            {
+                return View(tag);
+            }
+        }
+    }
+}
