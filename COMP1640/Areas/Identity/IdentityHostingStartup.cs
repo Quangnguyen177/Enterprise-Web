a3471f956b660e69c3c8306c9e9b6828ff8c080e
ducmtgch200681 <ducmtgch200681@fpt.edu.vn>
2023-02-15 09:35:39 +0700

    new project update
    fix bug appsetting
    add scaffolding

---
+++ COMP1640/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using COMP1640;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(COMP1640.Areas.Identity.IdentityHostingStartup))]
+namespace COMP1640.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
