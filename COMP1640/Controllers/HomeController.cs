a2476195b87863f02877b4c6e26ee95a5efe2d84
Harris <89270256+BeDevH@users.noreply.github.com>
2023-02-19 01:28:37 +0700

    Edit idea FE

--- COMP1640/Controllers/HomeController.cs
+++ COMP1640/Controllers/HomeController.cs
@@ -33,6 +33,11 @@ namespace COMP1640.Controllers
             return View();
         }
 
+        public IActionResult EditIdea()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
