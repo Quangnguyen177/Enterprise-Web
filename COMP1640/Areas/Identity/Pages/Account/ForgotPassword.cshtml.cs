48a9d96f360e09b2eff2a04ba4ddee9f472395d3
ducmtgch200681 <ducmtgch200681@fpt.edu.vn>
2023-03-02 23:14:11 +0700

    asp.net resend email and forgot password
    Remake the auto code for
    +) resend email confirm
    +) forgot password

--- COMP1640/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ COMP1640/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -10,16 +10,17 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using COMP1640.Models;
 
 namespace COMP1640.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class ForgotPasswordModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
+        private readonly UserManager<Models.Profile> _userManager;
         private readonly IEmailSender _emailSender;
 
-        public ForgotPasswordModel(UserManager<IdentityUser> userManager, IEmailSender emailSender)
+        public ForgotPasswordModel(UserManager<Models.Profile> userManager, IEmailSender emailSender)
         {
             _userManager = userManager;
             _emailSender = emailSender;
