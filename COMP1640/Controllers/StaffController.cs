4fca133d4e87aaeccb88834464cbc015e8241fa0
DanTruongdev <89248193+DanTruongdev@users.noreply.github.com>
2023-03-06 03:29:45 +0700

    Update Comment Function

--- COMP1640/Controllers/StaffController.cs
+++ COMP1640/Controllers/StaffController.cs
@@ -11,37 +11,47 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Http;
 using System.IO;
 using Newtonsoft.Json;
-using System.IO.Compression;
+using static Microsoft.EntityFrameworkCore.Internal.AsyncLock;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Identity;
 
 namespace COMP1640.Controllers
 {
     public class StaffController : Controller
     {
         private readonly ApplicationDbContext Db;
-        public StaffController(ApplicationDbContext context)
+        private readonly UserManager<Profile> _userManager;
+        public StaffController(ApplicationDbContext context, UserManager<Profile> userManager)
         {
             Db = context;
+            _userManager = userManager;
         }
 
         [HttpGet]
-        public IActionResult ViewPage(int pageNum)
+        public IActionResult ViewPage(int pageNum, string viewType)
         {
+            if (pageNum == 1) ViewBag.PageNum = 1;
+            else ViewBag.PageNum = pageNum;
             int skipPage = 5 * (pageNum - 1);
-            var page = Db.Ideas.Skip(skipPage).Take(5).ToList();
-            ViewBag.Page = pageNum;
+            List<Idea> page = null;
+            if (viewType.Equals("mostview"))
+            {
+                page = Db.Ideas.OrderByDescending(i => i.idea_view).Skip(skipPage).Take(5).ToList();
+                ViewBag.ViewType = "mostview";
+            }
+            else if (viewType.Equals("lastest"))
+            {
+                page = Db.Ideas.OrderByDescending(i => i.created_date).Skip(skipPage).Take(5).ToList();
+                ViewBag.ViewType = "lastest";
+            }
+            else if (viewType.Equals("popular"))
+            {
+                page = Db.Ideas.Include(i => i.Reacpoint).OrderByDescending(i => i.Reacpoint.ThumbDown + i.Reacpoint.ThumbUp).Skip(skipPage).Take(5).ToList();
+                ViewBag.ViewType = "lastest";
+            }
+            ViewBag.Category = Db.Categories.ToList();
             return View(page);
         }
-
-        //chua co truong view trong idea nen comment
-        /*[HttpGet]
-        public IActionResult MostView(int pageNum)
-        {
-            int skipPage = 5 * (pageNum - 1);
-            var ideasList = Db.Ideas.OrderByDescending(i => i.View);
-            var page = ideasList.Skip(skipPage).Take(5).ToList();
-            ViewBag.Page = pageNum;
-            return View(page);
-        }*/
 
         [HttpGet]
         public IActionResult LastComment(int pageNum)
@@ -52,7 +62,7 @@ namespace COMP1640.Controllers
             ViewBag.Page = pageNum;
             return View(page);
         }
-
+        
         [HttpGet]
         public IActionResult AddIdea()
         {
@@ -60,7 +70,7 @@ namespace COMP1640.Controllers
         }
 
         [HttpPost]
-        public IActionResult AddIdea(List<IFormFile> uploadedFiles, string title, string content, string categoryName, string isAnonymous, string profileId)
+        public IActionResult AddIdea(List<IFormFile> uploadedFiles,string title, string content, string categoryName, string isAnonymous, string profileId)
         {
             ////Create reactpoint entity to relate to Idea entity
             ReactPoint newReactPoint = new ReactPoint
@@ -93,13 +103,13 @@ namespace COMP1640.Controllers
             Db.SaveChanges();
             if (uploadedFiles.Count > 0)
             {
-                HandleFile(uploadedFiles, Db.Ideas.OrderBy(i => i.IdeaId).Last().IdeaId, "ADD"); //add file
+                HandleFile(uploadedFiles, Db.Ideas.OrderBy(i => i.IdeaId).Last().IdeaId,"ADD"); //add file
             }
             return View(Db.Categories.ToList());
 
         }
-
-        public void HandleFile(List<IFormFile> uploadedFiles, int idIdea, string type)
+            
+         public void HandleFile(List<IFormFile> uploadedFiles, int idIdea, string type)
         {
 
             foreach (var file in uploadedFiles)
@@ -122,18 +132,17 @@ namespace COMP1640.Controllers
                 Models.Document doc = new Models.Document() //đoạn này đang addd  dữ liệu vô database goodddd :)))
                 {
 
-                    doc_name = file.FileName,
-                    doc_path = path,
+                    doc_content = file.FileName,
+                    doc_type = "Still dont know",
                     IdeaId = idIdea
                 };
                 if (type.Equals("ADD"))
                 {
                     Db.Add(doc);
-                }
-                else if (type.Equals("UPDATE"))
+                } else if (type.Equals("UPDATE"))
                 {
                     Db.Update(doc);
-                }
+                }              
                 Db.SaveChanges();
             }
         }
@@ -142,7 +151,7 @@ namespace COMP1640.Controllers
         public IActionResult EditIdea(int id)
         {
             var a = id;
-            Idea currentIdea = Db.Ideas.FirstOrDefault(i => i.IdeaId == id);
+            Idea currentIdea = Db.Ideas.FirstOrDefault(i => i.IdeaId == id);           
             ViewBag.Category = Db.Categories.ToList();
             //if (currentIdea.first_closure != null)
             //    if (!CheckFirtClosureDate(currentIdea)){
@@ -150,8 +159,8 @@ namespace COMP1640.Controllers
             //        return null;
             return View(currentIdea);
         }
-
-        [HttpPost]
+        
+       [HttpPost]
         public IActionResult EditIdea(int ideaId, List<IFormFile> uploadedFiles, string title, string content, string categoryName, string isAnonymous)
         {
             bool anonynous = true;
@@ -174,97 +183,51 @@ namespace COMP1640.Controllers
         }
 
 
-
-
-
         //comment
-        public IActionResult Comment()
-        {
-            return View();
-        }
-
         [HttpPost]
-        public async Task<IActionResult> Comment(string content, string isAnonymous, int ideaId, string toEmail, string name)
+        public JsonResult Comment(Comment com)
         {
-            // send a notification mail to idea owner
-            var sendMail = SendNotificationEmail(name, toEmail, content);
-            if (sendMail) // check if sending mail is successfull!
+            //get user to use name and toEmail
+            var profile = Db.Profile.FirstOrDefault(u =>  u.Id == com.ProfileId);
+            var idea = Db.Ideas.Include(i => i.Profile).FirstOrDefault(i => i.IdeaId == com.IdeaId);
+            //default sender is system, so from email is not needed.
+            //var sendMail = SendEmail("dantruong2002tq@gmail.com", "Dan Truong", com.com_content);
+            var sendMail = SendEmail(idea.Profile.Email, idea.Profile.Name, com.com_content);
+            if (true) // check if sending mail is successfull!
             {
                 //add comment 
-                bool anonynous = true;
-                if (isAnonymous == null) anonynous = false;
                 Comment newComment = new Comment()
                 {
-                    com_content = content,
-                    ProfileId = "1",
-                    com_anonymous = anonynous,
-                    IdeaId = ideaId,
+                    com_content = com.com_content,
+                    ProfileId = com.ProfileId,
+                    Profile = profile,
+                    created_date = DateTime.Now,
+                    com_anonymous = com.com_anonymous,
+                    IdeaId = com.IdeaId,
+                    Idea = idea
                 };
                 Db.Add(newComment);
-                await Db.SaveChangesAsync();
+                Db.SaveChanges();
+                var result = new
+                {                 
+                    Name = profile.Name,
+                    Avatar = profile.Avatar,
+                    Anonymous = com.com_anonymous,
+                    Content = com.com_content,
+                    Time = newComment.created_date
+            };
+               
+                var response = JsonConvert.SerializeObject(result);
+                return Json(response);
             }
             else
             {
-                ViewBag.Error = "Comment failed!";
+                return Json(null);
             }
-            return View();
+
         }
 
-        [HttpPost]
-        public async Task<JsonResult> ReactPoint([FromBody] ReactPoint obj)
-        {
-            Db.ReactPoints.Update(obj);
-            await Db.SaveChangesAsync();
-            ReactPoint data = Db.ReactPoints.FirstOrDefault(o => o.ReactPointId == obj.ReactPointId);
-            string result = JsonConvert.SerializeObject(data);
-            return Json(result);
-        }
-
-        [HttpPost]
-        public async Task<JsonResult> React([FromBody] React save)
-        {
-            var check = Db.React.FirstOrDefault(r => r.ProfileId == save.ProfileId);
-
-            if (check != null)
-            {
-                check.Reacted = save.Reacted;
-                Db.React.Update(check);
-                await Db.SaveChangesAsync();
-            }
-            else if (check == null)
-            {
-                Db.React.Add(save);
-                await Db.SaveChangesAsync();
-            }
-            string result = JsonConvert.SerializeObject(save);
-            return Json(result);
-        }
-
-        [HttpPost]
-        public async Task<JsonResult> DeleteReact([FromBody] React save)
-        {
-            var the_idea = Db.React.Include(i => i.Idea).FirstOrDefault(i => i.IdeaId == save.IdeaId);
-            var the_profile = Db.React.Include(p => p.Profile).FirstOrDefault(p => p.ProfileId == save.ProfileId);
-            var the_result = Db.React.Where(a => a.IdeaId == the_idea.IdeaId).Where(b => b.ProfileId == the_profile.ProfileId).Single();
-
-            if (the_result != null)
-            {
-                Db.React.Remove(the_result);
-                await Db.SaveChangesAsync();
-            }
-            string result = JsonConvert.SerializeObject(save);
-            return Json(result);
-        }
-
-        //check if current time is earlier than 1st closure date 
-        public bool CheckFirtClosureDate(Idea idea)
-        {
-            DateTime firtClosureDate = DateTime.Parse(idea.first_closure.ToString()); //not accept nullable datetime type
-            if (DateTime.Compare(DateTime.Now, firtClosureDate) < 0) return true;
-            else return false;
-        }
-        [HttpPost]
-        public bool SendNotificationEmail(string name, string toEmail, string content)
+        public bool SendEmail(string toEmail, string name, string content)
         {
             try
             {
@@ -289,7 +252,7 @@ namespace COMP1640.Controllers
                 client.Port = 587;
                 client.UseDefaultCredentials = false;
                 // Set the credentials used to authenticate with the SMTP server (if required)
-                client.Credentials = new System.Net.NetworkCredential("truongtd2002tq@gmail.com", "dyxjomsrvccdgfys");
+                client.Credentials = new System.Net.NetworkCredential("truongtd2002tq@gmail.com", "uqjavdcqqzxrsjmo");
                 client.DeliveryMethod = SmtpDeliveryMethod.Network;
                 // Enable SSL if required
                 client.EnableSsl = true;
@@ -304,75 +267,121 @@ namespace COMP1640.Controllers
             }
         }
 
-        public IActionResult DetailIdea(int id)
+        [HttpPost]
+        public async Task<JsonResult> ReactPoint([FromBody] ReactPoint obj)
+        {
+            Db.ReactPoints.Update(obj);
+            await Db.SaveChangesAsync();
+            ReactPoint data = Db.ReactPoints.FirstOrDefault(o => o.ReactPointId == obj.ReactPointId);
+            string result = JsonConvert.SerializeObject(data);
+            return Json(result);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> React([FromBody] React save)
+        {
+            var check = Db.React.FirstOrDefault(r => r.ProfileId == save.ProfileId);
+
+            if ( check != null )
+            {                
+                check.Reacted = save.Reacted;
+                Db.React.Update(check);
+                await Db.SaveChangesAsync();
+            }
+            else if ( check == null)
+            {
+                Db.React.Add(save);
+                await Db.SaveChangesAsync();
+            }
+            string result = JsonConvert.SerializeObject(save);
+            return Json(result);
+        }
+
+        [HttpPost]
+        public async Task<JsonResult> DeleteReact([FromBody] React save)
+        {
+            var the_idea = Db.React.Include(i => i.Idea).FirstOrDefault(i => i.IdeaId == save.IdeaId);
+            var the_profile = Db.React.Include(p => p.Profile).FirstOrDefault(p => p.ProfileId == save.ProfileId);
+            var the_result = Db.React.Where(a => a.IdeaId == the_idea.IdeaId).Where(b => b.ProfileId == the_profile.ProfileId).Single();
+
+            if (the_result != null)
+            {
+                Db.React.Remove(the_result);
+                await Db.SaveChangesAsync();
+            }
+            string result = JsonConvert.SerializeObject(save);
+            return Json(result);
+        } 
+
+    //check if current time is earlier than 1st closure date 
+    public bool CheckFirtClosureDate(Idea idea)
+        {
+            DateTime firtClosureDate = DateTime.Parse(idea.first_closure.ToString()); //not accept nullable datetime type
+            if (DateTime.Compare(DateTime.Now, firtClosureDate) < 0) return true;
+            else return false;
+        }
+     
+
+        public async Task<IActionResult> DetailIdea(int id)
         {
 
             var user_of_idea = Db.Ideas.Include(u => u.Profile).FirstOrDefault(u => u.IdeaId == id);
             var name_of_user = user_of_idea.Profile.Name;
             ViewBag.Name = name_of_user;
 
-            var comments = Db.Comments.Include(p => p.Idea).FirstOrDefault(p => p.IdeaId == id);
-            ViewBag.Comments = Db.Comments.Where(comments => comments.IdeaId == id).ToList();
+            //su dung lenh nay cua duc khong hien thi dung ten nguoi comment.
+            //var comments = Db.Comments.Include(p => p.Idea).FirstOrDefault(p => p.IdeaId == id);
+            //ViewBag.Comments = Db.Comments.Where(comments => comments.IdeaId == id).ToList();
+            
+            //lenh thay the: truong commmit
+            var comments = Db.Comments.Include(c => c.Idea);
+            ViewBag.Comments = Db.Comments.Where(comments => comments.IdeaId == id).Include(c => c.Profile).ToList();
 
             var documents = Db.Documents.Include(d => d.Idea).FirstOrDefault(d => d.IdeaId == id);
             ViewBag.Documents = Db.Documents.Where(documents => documents.IdeaId == id).ToList();
 
+            var idea = Db.Ideas.Include(c => c.Category).FirstOrDefault(c => c.IdeaId == id);
+
+            //ducmt1
             var reactpoint_of_idea = Db.Ideas.Include(r => r.Reacpoint).FirstOrDefault(u => u.IdeaId == id);
             var number_of_upvote = reactpoint_of_idea.Reacpoint.ThumbUp;
             ViewBag.ThumbUp = number_of_upvote;
             var number_of_downvote = reactpoint_of_idea.Reacpoint.ThumbDown;
             ViewBag.ThumbDown = number_of_downvote;
 
-            var idea = Db.Ideas.Include(c => c.Category).FirstOrDefault(c => c.IdeaId == id);
+
+            //ducmt2
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user_react = Db.React.Include(p => p.Profile).FirstOrDefault(p => p.ProfileId == userId);
+            var idea_react = Db.React.Include(i => i.Idea).FirstOrDefault(i => i.IdeaId == id);
+
+            try
+            {
+                var the_react = Db.React.Where(a => a.IdeaId == idea_react.IdeaId).Where(b => b.ProfileId == user_react.ProfileId).Single();
+                ViewBag.UserReact = the_react.Reacted; 
+            }
+            catch (Exception ex)
+            {
+                ViewBag.UserReact = null;
+            }
+
             return View(idea);
         }
 
-        public IActionResult DownloadFile(int id)
+        public FileResult DownloadFile(int id)
         {
             var file = Db.Documents.Find(id);
-            var filename = file.doc_name;
-            List<Document> listFiles = new List<Document>();
+            var filename = file.doc_content;
+            return File(@"F:\\F2G - UOG\\COMP1640 - Enterprise Web Software Development", filename, System.Net.Mime.MediaTypeNames.Application.Octet);
+        }
 
-            //Path For download From Network Path.
-            string fileSavePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Files"); ;
-
-            DirectoryInfo dirInfo = new DirectoryInfo(fileSavePath);
-
-            int i = 0;
-
-            foreach (var item in dirInfo.GetFiles())
-            {
-                if (item.Name == filename)
-                {
-                    listFiles.Add(new Document()
-                    {
-
-                        DocId = i + 1,
-
-                        doc_name = item.Name,
-
-                        doc_path = dirInfo.FullName + @"\" + item.Name
-
-                    });
-                }
-
-
-                i = i + 1;
-            }
-            var fileColumns = listFiles.ToList();
-            using (var memoryStream = new MemoryStream())
-            {
-                using (var ziparchive = new ZipArchive(memoryStream, ZipArchiveMode.Create, true))
-                {
-                    for (int j = 0; j < fileColumns.Count; j++)
-                    {
-                        ziparchive.CreateEntryFromFile(fileColumns[j].doc_path, fileColumns[j].doc_name);
-
-                    }
-                }
-
-                return File(memoryStream.ToArray(), "application/zip", "Documents.zip");
-            }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
         }
 
         //public bool CheckFinalClosureDate(int ideaId)
@@ -381,19 +390,6 @@ namespace COMP1640.Controllers
         //    DateTime finalClosureDate = DateTime.Parse(idea.first_closure.ToString()); //not accept nullable datetime type
         //    if (DateTime.Compare(DateTime.Now, finalClosureDate) < 0) return true;
         //    else return false;
-        //}
-
-
-        // !!! This is the update account profile system !!!
-        // Different with profile page
-        // More info at Identity/Account/Manage
-
-        //[HttpPost]
-        //public async Task<IActionResult> UpdateAccount(Profile pro)
-        //{
-        //    Db.Profile.Update(pro);
-        //    await Db.SaveChangesAsync();
-        //    return View(pro);
         //}
     }
 }
